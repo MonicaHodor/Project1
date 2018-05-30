ccb2051d02042509b51d2722a63ce404569f753f
FaragauL <inureanlavinia@gmail.com>
2018-05-30 12:12:54 +0300

    added new class

--- src/main/java/org/fasttrackit/NewClass.java
+++ src/main/java/org/fasttrackit/NewClass.java
@@ -3,6 +3,6 @@ package org.fasttrackit;
 public class NewClass {
     public static void main( String[] args )
     {
-        System.out.println("Hello World!");
+        System.out.println("Hello  World!");
     }
 }
