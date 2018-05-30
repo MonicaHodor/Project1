f47f17a73f9d3919c89d4cb46c8a71a31952e7c7
MonicaHodor <monicahodor32@gmail.com>
2018-05-30 13:27:47 +0300

    Another log

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -12,7 +12,7 @@ public class App
         System.out.println( "Hello New World!" );
         System.out.println( "Hello Monica!" );
         System.out.println( "Good day!" );
-        System.out.println( "A day" );
+
         System.out.println( "Have a nice day! :)" );
     }
 
