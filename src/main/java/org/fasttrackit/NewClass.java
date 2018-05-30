f2a079c3d791079aa48934ce1607508c20911958
MonicaHodor <monicahodor32@gmail.com>
2018-05-30 13:14:25 +0300

    New class log

--- src/main/java/org/fasttrackit/NewClass.java
+++ src/main/java/org/fasttrackit/NewClass.java
@@ -4,6 +4,6 @@ public class NewClass {
     public static void main( String[] args )
     {
         System.out.println("Hello World!");
-        System.out.println("Hello again!");
+        System.out.println("Hello  again!");
     }
 }
