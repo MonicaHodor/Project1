d4f80ebd8ff8b44cbed8f15e58a5e64b02d80d26
FaragauL <inureanlavinia@gmail.com>
2018-05-30 13:24:43 +0300

    added new class

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -10,8 +10,7 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "Hello New World!" );
-        System.out.println( "Hello Monica!" );
-        System.out.println( "Good day!" );
+        System.out.println( "Have a nice day! :)" );
     }
 
 }
